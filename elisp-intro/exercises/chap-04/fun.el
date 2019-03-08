0f25aaeb59d7b097905f593f8ddefa563935c249
lifeich1 <lifeich0@gmail.com>
2019-03-08 10:41:55 +0800

    :white_check_mark: exercise 4.6.2

--- elisp-intro/exercises/chap-04/fun.el
+++ elisp-intro/exercises/chap-04/fun.el
@@ -7,3 +7,16 @@ Write your own ‘simplified-end-of-buffer’ function definition; then
 test it to see whether it works."
   (interactive)
   (goto-char (point-max)))
+
+
+(defun test-buffer-exists (buffer-or-name)
+  "print message telling BUFFER-OR-NAME is an existing buffer.
+
+Use ‘if’ and ‘get-buffer’ to write a function that prints a message
+telling you whether a buffer exists."
+  (interactive "Bbuffer name: ")
+  (if (bufferp (get-buffer buffer-or-name))
+      (message "buffer %s exists" buffer-or-name)
+    (message "cannot find buffer %s" buffer-or-name)))
+
+(test-buffer-exists "no_here.txt")
